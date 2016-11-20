6246ac8703f8be9f8d3f3ca5d96a68d1b4893d61
Ross Eastman <ross.eastman@mayden.co.uk>
2016-11-20 16:01:42 +0000

    added settings and service factory

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,17 @@ return [
             'path' => __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        //Database details
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'database',
+            'username' => 'user',
+            'password' => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
     ],
 ];
