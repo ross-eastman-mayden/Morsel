be16c72aa4608848d986c49552826d0471d838ca
Ross Eastman <ross.eastman@mayden.co.uk>
2016-11-20 15:38:31 +0000

    added routes to routes.php and removed proposed routes (new route http complient)

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,27 @@
 <?php
 // Routes
-
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->post('/booking', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    $this->logger->info("Morsel 'post' add Booking route");
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+$app->get('/booking/[{id}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Morsel 'get' show Booking Details route");
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+$app->put('/booking', function ($request, $response, $args) {
+    // Sample log messagx§e
+    $this->logger->info("Morsel 'put' update Booking Details route");
 
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+$app->delete('/booking/[{id}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Morsel 'delete' cancel Booking route");
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
