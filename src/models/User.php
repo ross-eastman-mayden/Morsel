8348de6bdb87d3d07e556c11e335aba5ee94b0d8
Ross Eastman <ross.eastman@mayden.co.uk>
2016-11-30 16:33:40 +0000

    added route namespace

---
+++ src/models/User.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Morsel\Models;
+
+
+class User
+{
+    public $id;
+    private $name;
+    private $address;
+    private $mobile;
+    private $homeNumber = 0;
+    private $email;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * @param mixed $mobile
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHomeNumber()
+    {
+        return $this->homeNumber;
+    }
+
+    /**
+     * @param int $homeNumber
+     */
+    public function setHomeNumber($homeNumber)
+    {
+        $this->homeNumber = $homeNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
