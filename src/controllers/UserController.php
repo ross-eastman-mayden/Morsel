4bace9c22adb0425e5f57ac6a9a7983e56cee00a
Ross Eastman <ross.eastman@mayden.co.uk>
2016-11-30 16:31:44 +0000

    added userController outline

---
+++ src/controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Morsel\Controller;
+
+
+class UserController
+{
+    // Get User model
+    // Insert into db
+
+    // Get User model
+    // Return to view template
+
+    // Get User model
+    // Change record in db
+
+    // Get User model
+    // archive in database
+
+}
